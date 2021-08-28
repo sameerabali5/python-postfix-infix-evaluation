44077d4d1fa3b1ad0b30e44ac4b716d9adf8f72d
sameerabali5 <89704491+sameerabali5@users.noreply.github.com>
2021-08-28 11:47:18 -0700

    Add files via upload

---
+++ pitests.py
@@ -0,0 +1,128 @@
+# Name:         Sameera Balijepalli
+# Course:       CPE 202
+# Instructor:   Daniel Kauffman
+# Assignment:   Postfix-It
+# Term:         Spring 2021
+
+import unittest
+
+import postfixit
+
+
+class TestOperand(unittest.TestCase):
+    def test_operand_1(self):
+        item = "0"
+        self.assertEqual(postfixit.operand(item), True)
+
+    def test_operand_2(self):
+        item = "1"
+        self.assertEqual(postfixit.operand(item), True)
+
+    def test_operand_3(self):
+        item = "-5"
+        self.assertEqual(postfixit.operand(item), True)
+
+    def test_operand_4(self):
+        item = "A"
+        self.assertEqual(postfixit.operand(item), False)
+
+    def test_operand_5(self):
+        item = "C"
+        self.assertEqual(postfixit.operand(item), False)
+
+    def test_operand_6(self):
+        item = "3.14"
+        self.assertEqual(postfixit.operand(item), True)
+
+
+class TestCalculate(unittest.TestCase):
+    def test_calculate_1(self):
+        self.assertEqual(postfixit.calculate(3.0, 1.0, "*"), 3.000)
+
+    def test_calculate_2(self):
+        self.assertEqual(postfixit.calculate(3.0, 1.0, "+"), 4.000)
+
+    def test_calculate_3(self):
+        self.assertEqual(postfixit.calculate(1.0, 3.0, "/"), 3.000)
+
+    def test_calculate_4(self):
+        self.assertEqual(postfixit.calculate(3.0, 1.0, "-"), -2.000)
+
+    def test_calculate_5(self):
+        self.assertEqual(postfixit.calculate(0.0, 1.0, "*"), 0.000)
+
+
+class TestPemdas(unittest.TestCase):
+    def test_pemdas_1(self):
+        item = "*"
+        self.assertEqual(postfixit.pemdas(item), 1)
+
+    def test_pemdas_2(self):
+        item = "+"
+        self.assertEqual(postfixit.pemdas(item), 0)
+
+    def test_pemdas_3(self):
+        item = "/"
+        self.assertEqual(postfixit.pemdas(item), 1)
+
+    def test_pemdas_4(self):
+        item = "-"
+        self.assertEqual(postfixit.pemdas(item), 0)
+
+    def test_pemdas_5(self):
+        item = "A"
+        self.assertEqual(postfixit.pemdas(item), -1)
+
+
+class TestPostfix(unittest.TestCase):
+    def test_postfix_1(self):
+        infix = "3 + 4"
+        self.assertEqual(postfixit.postfix(infix),
+                            "3 4 +")
+
+    def test_postfix_2(self):
+        infix = "( 1 + 2 ) * 3"
+        self.assertEqual(postfixit.postfix(infix),
+                            "1 2 + 3 *")
+
+    def test_postfix_3(self):
+        infix = "8 / 4 ^ 3"
+        self.assertEqual(postfixit.postfix(infix),
+                            "8 4 3 ^ /")
+
+    def test_postfix_4(self):
+        infix = "( 3 + 4 ) - 5"
+        self.assertEqual(postfixit.postfix(infix),
+                            "3 4 + 5 -")
+
+    def test_postfix_5(self):
+        infix = "( 3 + 4 ) ^ 7"
+        self.assertEqual(postfixit.postfix(infix),
+                            "3 4 + 7 ^")
+
+
+class TestEvaluate(unittest.TestCase):
+    def test_evaluate_1(self):
+        postfix = "3 4 +"
+        self.assertEqual(postfixit.evaluate(postfix),
+                            "7.000")
+    def test_evaluate_2(self):
+        postfix = "1 2 + 3 *"
+        self.assertEqual(postfixit.evaluate(postfix),
+                            "9.000")
+    def test_evaluate_3(self):
+        postfix = "8 4 3 ^ /"
+        self.assertEqual(postfixit.evaluate(postfix),
+                            "0.125")
+    def test_evaluate_4(self):
+        postfix = "3 4 + 5 -"
+        self.assertEqual(postfixit.evaluate(postfix),
+                            "2.000")
+    def test_evaluate_5(self):
+        postfix = "3 4 + 7 ^"
+        self.assertEqual(postfixit.evaluate(postfix),
+                            "823543.000")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ postfixit.py
@@ -0,0 +1,124 @@
+# Name:         Sameera Balijepalli
+# Course:       CPE 202
+# Instructor:   Daniel Kauffman
+# Assignment:   Postfix-It
+# Term:         Spring 2021
+
+#from typing import List
+
+
+def main() -> None:
+    """
+    Iteratively prompt the user for an infix expression and display both the
+    postfix equivalent and, on the next line, the result as a float (even if it
+    is a whole number) rounded to 3 decimal places. Assume the infix expression
+    is properly formatted.
+    """
+    while True:
+        try:
+            infix = input(">>> ")
+            # insert all code for main below this line
+            infix_convert = postfix(infix)
+            print(infix_convert)
+            print(evaluate(infix_convert))
+
+        except EOFError:  # loop breaks with CTRL+d
+            break
+    print()  # empty line prints before program ends
+    # end of main (no return statement is equivalent to |return None|)
+
+
+# insert additional function definitions below this line
+def operand(item: str) -> bool:
+    """Checks if string contains an operator (typically a variable).
+    Returns True if yes, False otherwise
+    """
+    for char in item:
+        if char == ".":
+            return item.replace(char, '', 1).isdigit()
+        elif char == "-":
+            return item.strip(char).isdigit()
+    return item.isdigit()
+
+
+def pemdas(item: str) -> int:
+    """Assigns precedence to operators"""
+    if item == "+":
+        return 0
+    elif item == "-":
+        return 0
+    elif item == "/":
+        return 1
+    elif item == "*":
+        return 1
+    elif item == "^":
+        return 2
+    return -1
+
+
+def postfix(infix: str) -> str:
+    """Converts infix expression to postfix expression"""
+    stack = [] #empty stack
+    postfix_out = " " #empty output
+    convert = infix.split()
+    for item in convert:
+        if operand(item):
+            postfix_out += item + " "
+        elif item == "(" or item == "^":
+            stack.append(item)
+        elif item == ")":
+            if len(stack) > 0:
+                while stack[-1] != "(":
+                    postfix_out += stack.pop() + " "
+            if len(stack) > 0:
+                stack.pop()
+        else:
+            while len(stack) > 0 and stack[-1] != "(" and \
+            pemdas(item) <= pemdas(stack[-1]):
+                if len(stack) > 0:
+                    postfix_out += stack.pop() + " "
+            stack.append(item)
+    while len(stack) > 0:
+        postfix_out += stack.pop() + " "
+    return "".join(postfix_out.strip())
+
+
+def calculate(right_pop: float, left_pop: float, item: str) -> float:
+    """Function performs caculators between operators."""
+    result = 0
+    if item == "/":
+        if right_pop != 0:
+            result = left_pop / right_pop
+    elif item == "*":
+        result = left_pop * right_pop
+    elif item == "+":
+        result = left_pop + right_pop
+    elif item == "-":
+        result = left_pop - right_pop
+    elif item == "^":
+        result = left_pop ** right_pop
+    return result
+
+
+def evaluate(postfix: str) -> str:
+    """Evaluates postfix expression"""
+    stack = []
+    evaluate = postfix.split()
+    for item in evaluate:
+        if operand(item):
+            stack.append(float(item))
+        else:
+            if len(stack) > 0:
+                right_pop = stack.pop()
+                left_pop = stack.pop()
+                result = calculate(right_pop, left_pop, item)
+                stack.append(float(result))
+    final = 0
+    if len(stack) > 0:
+        final = stack.pop()
+    return format(final, '.3f')
+
+
+# do not add code below this line
+if __name__ == "__main__":  # runs main with command |python3 postfixit.py|
+    main()
